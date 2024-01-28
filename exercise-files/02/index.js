a4b96ab88c0933fcce56fadfadf1368b206dbd12
Sandy Instructor <sandyludosky@gmail.com>
2024-01-28 14:29:49 +0100

    02_05

--- exercise-files/02/index.js
+++ exercise-files/02/index.js
@@ -3,8 +3,12 @@ const OpenAI = require("openai");
 const readlineSync = require("readline-sync");
 
 // Open AI configuration
-let messages = [{ role: "system", content: "You are a funny assistant." }]
+
 const openai = new OpenAI();
+const PERSONA = "You are a skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
+const MODEL_ENGINE = "gpt-3.5-turbo"
+const MESSAGE_SYSTEM = " You are a skilled stand-up comedian with a knack for telling 1-2 sentence funny stories."
+let messages = [{"role": "system", "content": MESSAGE_SYSTEM}]
 
 // Get user input
 function getInput(promptMessage) {
@@ -28,13 +32,16 @@ async function runConversation() {
       console.log("Goodbye!");
       process.exit();
     }
-    messages.push({ role: "user", content: input });
+    messages.push({"role": "user", "content": "Tell me a joke about Parisian people."})
+    
+    // generate completions
     const completion = await openai.chat.completions.create({
-      messages: messages,
-      model: "gpt-3.5-turbo",
+    messages: messages,
+    model: MODEL_ENGINE,
     });
-    messages.push(completion.choices[0].message);
-    console.log(completion.choices[0].message.content);
+    messages.push({"role": "user", "content": completion.choices[0].message})
+    console.log("Bot: " + completion.choices[0].message.content);
+    console.log("Usage: " + completion.usage.total_tokens + " tokens used");
   }
 }
 
