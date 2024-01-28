3503ea39e9bf621e4e088e63494ce42ca0148b63
Sandy Instructor <sandyludosky@gmail.com>
2024-01-28 18:29:10 +0100

    03_04b

--- exercise-files/03/index.js
+++ exercise-files/03/index.js
@@ -1,20 +1,18 @@
-require("dotenv").config();
-const OpenAI = require("openai");
-const readlineSync = require("readline-sync");
+import OpenAI from "openai";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+import readlineSync from "readline-sync";
+dotenv.config();
 
-// Open AI configuration
-const MESSAGE_SYSTEM = "You are helpful and professional assistant."
-const MODEL_ENGINE = "gpt-3.5-turbo"
-let messages = [{ role: "system", content: MESSAGE_SYSTEM }]
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-// Get user input
 function getInput(promptMessage) {
   return readlineSync.question(promptMessage, {
     hideEchoBack: false, // The typed characters won't be displayed if set to true
   });
 }
-
 
 function kelvinToCelsius(kelvin) {
   return JSON.stringify(Math.round(kelvin - 273.15));
@@ -44,11 +42,9 @@ async function getCurrentWeather(location, unit = "fahrenheit") {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
   );
-
   console.log(response)
-  //parse the response and return the weather info as a JSON object
 
-  // ...
+  // documentation : https://openweathermap.org/api/one-call-3
   const weatherInfo = {
     location: "Paris",
     temperature: "22",
@@ -58,18 +54,7 @@ async function getCurrentWeather(location, unit = "fahrenheit") {
   return JSON.stringify(weatherInfo);
 }
 
-
-
-async function main() {
-  console.log("\n\n----------------------------------");
-  console.log("          CHAT WITH AI 🤖   ");
-  console.log("----------------------------------\n");
-  console.log("type 'x' to exit the conversation");
-  runConversation();
-}
-
-async function runConversation() {
-  // Step 1: send the conversation and available functions to the model
+async function runConversation(messages) {
 
   const tools = [
     {
@@ -98,39 +83,91 @@ async function runConversation() {
       console.log("Goodbye!");
       process.exit();
     }
-    messages.push({ role: "user", content: input });
+  messages.push({"role": "user", "content": input})
     
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
-      messages: messages,
-      tools: tools,
-      tool_choice: "auto", // auto is default, but we'll be explicit
-    });
-    const responseMessage = response.choices[0].message;
-
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-1106",
+    messages: messages,
+    tools: tools,
+    tool_choice: "auto", // auto is default, but we'll be explicit
+  });
+  const responseMessage = response.choices[0].message;
   // Step 2: check if the model wanted to call a function
   const toolCalls = responseMessage.tool_calls;
-  if (responseMessage.tool_calls) {
-    // Step 3: call the function
-    // Note: the JSON response may not always be valid; be sure to handle errors
-    const availableFunctions = {
-      get_current_weather: getCurrentWeather,
-    }; // only one function in this example, but you can have multiple
-    messages.push(responseMessage); // extend conversation with assistant's reply
-    for (const toolCall of toolCalls) {
-      const functionName = toolCall.function.name;
-      const functionToCall = availableFunctions[functionName];
-      const functionArgs = JSON.parse(toolCall.function.arguments);
-      const functionResponse = functionToCall(
-        functionArgs.location,
-        functionArgs.unit
-      );
-      console.log(functionResponse)
-      // Step 4: append the JSON object to the conversation
+  if (!responseMessage.tool_calls) {
+    return response;
+  }
+  // Step 3: call the function
+  // Note: the JSON response may not always be valid; be sure to handle errors
+  const availableFunctions = {
+    get_current_weather: getCurrentWeather,
+  }; // only one function in this example, but you can have multiple
+  messages.push(responseMessage); // extend conversation with assistant's reply
+  for (const toolCall of toolCalls) {
+    const functionName = toolCall.function.name;
+    const functionToCall = availableFunctions[functionName];
+    const functionArgs = JSON.parse(toolCall.function.arguments);
+    const functionResponse = await functionToCall(
+      functionArgs.location,
+      functionArgs.unit
+    );
+    console.log(functionResponse)
+    // Step 4: send the info on the function call and function response to GPT
+    // extend conversation with function response
+    // messages.push({
+    //   tool_call_id: toolCall.id,
+    //   role: "tool",
+    //   name: functionName,
+    //   content: functionResponse,
+    // }); // extend conversation with function response
+    try {
+      // const secondResponse = await openai.chat.completions.create({
+      //   model: "gpt-3.5-turbo-1106",
+      //   messages: messages,
+      // }); // get a new response from the model where it can see the function response
+      // return secondResponse;
+    } catch (e) {
+      console.error(e);
     }
-     // Step 5: extend the conversation with the function's response
-    }
+  }
   }
 }
 
-main(); 
+() => {
+  geoCode("Boston");
+};
+
+const printMessage = (response) => {
+  console.log(`\nBot: ${response.choices[0].message.content}\x1b`);
+  main();
+};
+
+const start = () => {
+  console.log("\n\n----------------------------------");
+  console.log("          CHAT WITH AI 🤖   ");
+  console.log("----------------------------------\n");
+  console.log("\nBot: How can I help you?");
+  main();
+};
+
+async function main() {
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a helpful assistant capable of telling the current weather",
+    },
+  ];
+  const input = getInput("You: ");
+
+  try {
+    if (!!input) {
+      messages.push({ role: "user", content: input });
+      runConversation(messages).then(printMessage).catch(console.error);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+start();
