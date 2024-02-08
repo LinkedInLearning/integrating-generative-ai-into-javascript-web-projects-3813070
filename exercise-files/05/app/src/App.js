8ecd258800b0f6499556d37643b4de30c764c8c5
Sandy Instructor <sandyludosky@gmail.com>
2024-02-08 12:50:24 +0100

    removed footer

--- exercise-files/05/app/src/App.js
+++ exercise-files/05/app/src/App.js
@@ -196,11 +196,7 @@ function App() {
           </form>
         </div>
       </div>
-
-      {/* <footer className="d-flex align-items-center justify-content-center position-relative bottom-0">
-        <div className="text-center">© 2020 Copyright Sajjad Hussain</div>
-      </footer> */}
-
+      
       <script src="https://cdnjs.cloudflare.com/ajax/libs/material-ui/5.0.0-beta.5/index.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/material-ui/5.0.0-beta.5/index.js"></script>
